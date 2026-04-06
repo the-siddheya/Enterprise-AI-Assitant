6fc5036abc8541b87e068d86977d37797ae401e4
the-siddheya <siddheyapitambare@gmail.com>
2026-04-06 17:42:44 +0530

    in login page email initialize

--- auth/login.py
+++ auth/login.py
@@ -6,6 +6,7 @@ def login():
 
     # Create input fields for user login
     username = st.text_input("Username")
+    email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
