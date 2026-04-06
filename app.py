3a28a46ae19cf816f13c533abc692c81335ce8cb
the-siddheya <siddheyapitambare@gmail.com>
2026-04-06 21:19:55 +0530

    app.py login and register error solve

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 import streamlit as st
-import auth.login as login
-import auth.register as register
+from auth.login import login
+from auth.register import register
 
 if "logged_in" not in st.session_state:
     st.session_state["logged_in"] = False
 
 if not st.session_state["logged_in"]:
     page = st.sidebar.selectbox("Select Page", ["Login", "Register"])
+
     if page == "Login":
-        login()  
+        login()   
     else:
-        register()
+        register()  
+
 else:
     if st.session_state["role"] == "admin":
         st.write("admin panel")
