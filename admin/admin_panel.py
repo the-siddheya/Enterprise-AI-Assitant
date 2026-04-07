11f90482c2181b9cb7e69fa39aa045fe408cf0b2
the-siddheya <siddheyapitambare@gmail.com>
2026-04-07 15:44:00 +0530

    created documents.csv for keep entery of file and status

--- admin/admin_panel.py
+++ admin/admin_panel.py
@@ -1,5 +1,8 @@
 import streamlit as st
 import os
+import pandas as pd
+
+DOCUMENTS_FILE = "documents.csv"
 
 def admin_panel():
     st.title("Admin Panel")
@@ -20,6 +23,15 @@ def admin_panel():
 
         with open(save_path, "wb") as f:
             f.write(uploaded_file.getbuffer())
+        
+        df = pd.read_csv(DOCUMENTS_FILE)
+        new_doc = {
+            "filename": uploaded_file.name,
+            "status": "pending"
+        }
+
+        df = pd.concat([df, pd.DataFrame([new_doc])], ignore_index=True)
+        df.to_csv(DOCUMENTS_FILE, index=False)
 
         st.success("File uploaded successfully!")
         st.write("File saved at:", save_path)
