0ae3e24f273925d886daaf4c0f612758f45ac942
the-siddheya <siddheyapitambare@gmail.com>
2026-04-07 15:17:25 +0530

    Updated Error Free code

--- auth/login.py
+++ auth/login.py
@@ -2,23 +2,29 @@ import streamlit as st
 import pandas as pd
 
 def login():
+
     st.title("Login")
 
-    # Create input fields for user login
-    username = st.text_input("Username")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
+
         df = pd.read_csv("users.csv")
-        user = df[(df["username"] == username) & (df["password"] == password)]
-        if len(user) > 0:
-            role = user.iloc[0]["role"]
+
+        found = False
+        role = "user"
+
+        for index in df.index:
+            row = df.loc[index]
+            if row["email"] == email and str(row["password"]) == password:
+                found = True
+                role = row["role"]
+                break
+        if found:
             st.session_state["logged_in"] = True
             st.session_state["role"] = role
             st.session_state["email"] = email
-
-            st.success(f"Login successful! Welcome, {username}.")
+            st.success("Login Successful")
         else:
-            st.error("Invalid username or password. Please try again.")
-
+            st.error("Invalid Credentials")
--- auth/register.py
+++ auth/register.py
@@ -1,38 +1,22 @@
 import streamlit as st
 import pandas as pd
-import os
 
 def register():
     st.title("Register")
 
-    username = st.text_input("Username")
+    name = st.text_input("Name")
     email = st.text_input("Email")
     password = st.text_input("Password", type="password")
 
     if st.button("Register"):
-
-        if not username or not email or not password:
-            st.warning("Please fill all fields")
-            return
-
-        if os.path.exists("users.csv"):
-            df = pd.read_csv("users.csv")
-        else:
-            df = pd.DataFrame(columns=["username", "email", "password", "role"])
-
-        # Check duplicate username
-        if (df["username"] == username).any():
-            st.error("Username already exists")
-            return
-
+        df = pd.read_csv("users.csv")
         new_user = {
-            "username": username,
-            "email": email,
-            "password": password,
-            "role": "user"
+            "name":name,
+            "email":email,
+            "password":password,
+            "role":"user"
         }
-
         df = pd.concat([df, pd.DataFrame([new_user])], ignore_index=True)
         df.to_csv("users.csv", index=False)
 
-        st.success("Registration successful! You can now log in.")
+        st.success("User Registered")
